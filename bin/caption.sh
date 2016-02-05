e1ee167fd0f2bee1d17c83a1c108035564bb0c82
Zach Tatlock <ztatlock@cs.washington.edu>
2016-02-04 19:32:05 -0800

    update caption script

--- bin/caption.sh
+++ bin/caption.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-TTF="~/Dropbox/notes/impact.ttf"
+TTF="$HOME/dotfiles/content/impact.ttf"
 
-if [ $# -lt 2 ]; then
+if [ ! -f "$TTF" ]; then
+  echo "Error: could not find impact.ttf"
+  exit 1
+fi
+
+if [ $# -lt 4 ]; then
   echo "Usage: $0 <img> <top-cap> <bot-cap> <font-size>"
   exit
 fi
@@ -13,7 +18,7 @@ BOT="$3"
 SIZ="$4"
 
 convert "$IMG" \
-  -fill white -stroke black \
+  -fill white -stroke black -strokewidth 5 \
   -font "$TTF" -pointsize $SIZ \
   -gravity North -annotate 0 "$TOP" \
   -gravity South -annotate 0 "$BOT" \
