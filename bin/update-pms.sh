1dc807da37886fe1ff21b0092930556666625965
Zach Tatlock <ztatlock@cs.washington.edu>
2017-12-09 03:17:25 -0500

    fix branch in update script

--- bin/update-pms.sh
+++ bin/update-pms.sh
@@ -3,15 +3,19 @@
 # exit on error
 set -e
 
+echo "Brew:"
+
 brew update
 brew upgrade
 
 # occasional clean up
-if [ "$(bc <<< $(date +"%e") % 3)" -eq 0 ]; then
+if [ $(($(date +%e) % 3)) -eq 0 ]; then
   brew cleanup
   brew prune
   brew doctor
 fi
+
+echo "OPAM:"
 
 opam update
 opam upgrade --yes
