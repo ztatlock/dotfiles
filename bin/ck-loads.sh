51a379eabba4e8c13a516ceaaa6c40a0e4b9fcf2
Zach Tatlock <ztatlock@cs.washington.edu>
2017-12-19 10:11:39 -0800

    add number of procs to load checker

--- bin/ck-loads.sh
+++ bin/ck-loads.sh
@@ -1,29 +1,40 @@
 #!/usr/bin/env bash
 
 readonly HOSTS="
-warfa.cs.washington.edu
 recycle.cs.washington.edu
 bicycle.cs.washington.edu
 tricycle.cs.washington.edu
 boom.cs.washington.edu
 bam.cs.washington.edu
 barb.cs.washington.edu
+warfa.cs.washington.edu
 buffalo.cs.washington.edu
 caribou.cs.washington.edu
 plover.cs.washington.edu
 "
 
 function main {
+  local h
   for h in $HOSTS; do
     local name="$(echo "$h" | cut -d '.' -f 1)"
-    local load="$(load $h)"
+    local proc="$(get_proc $h)"
+    local load="$(get_load $h)"
 
-    printf "%10s %s\n" "$name" "$load"
+    printf "%10s %4d %s\n" \
+      "$name" "$proc" "$load"
   done
 }
 
-function load {
-  ssh "$1" uptime   \
+function get_proc {
+  local host="$1"
+
+  ssh "$host" "nproc --all"
+}
+
+function get_load {
+  local host="$1"
+
+  ssh "$host" uptime   \
     | sed 's/.*://' \
     | sed 's/,//g'  \
     | xargs printf "%7.2f"
