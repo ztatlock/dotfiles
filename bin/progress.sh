ebe3e286707345d0cebca2bebd0831648e12cdab
Zach Tatlock <ztatlock@cs.washington.edu>
2018-08-27 10:11:56 -0700

    progress log script now prompts to set up template if none found

--- bin/progress.sh
+++ bin/progress.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env bash
 
-P="$HOME/Dropbox/progress/$(gdate +'%y%m%d' -d 'last saturday').md"
+P="$HOME/Dropbox/progress"
+T="$P/TEMPLATE.md"
+W="$P/$(gdate +'%y%m%d' -d 'last saturday').md"
 
-$EDITOR "$P"
+if [ ! -d "$P" ]; then
+  echo "Progress log directory not found!"
+  exit 1
+fi
+
+if [ ! -f "$W" ]; then
+  echo "Progress log for this week not found."
+  read -p "Set up from template (y/n)? " yn
+  case $yn in
+    y)
+      cp "$T" "$W"
+      ;;
+    *)
+      echo "No changes made."
+      exit 0
+      ;;
+  esac
+fi
+
+$EDITOR "$W"
