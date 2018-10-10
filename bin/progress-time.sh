72a0b2a0d15bf8ceb0d84cae5d1d511592cf1267
Zach Tatlock <ztatlock@cs.washington.edu>
2018-10-10 08:41:44 -0700

    fix spreadsheet mode time tally

--- bin/progress-time.sh
+++ bin/progress-time.sh
@@ -1,29 +1,47 @@
 #!/usr/bin/env bash
 
 MODE="PROGRESS"
-if [ "$1" = "-t" ]; then
-  MODE="TABS"
+if [ "$1" = "-s" ]; then
+  MODE="SPREADSHEET"
 fi
 
 CODELINE="$(mktemp)"
 
 cat > "$CODELINE"
 
+FIRST=true
 function tally {
   t=$(cat "$CODELINE" \
       | fold -w 1 \
       | grep "$1" \
       | awk 'END {print NR / 4}')
-  if [ "$MODE" = "TABS" ]; then
-    printf "%0.2f\t" "$t"
-  else
-    if [ $(echo "$t > 0" | bc -l) = "1" ]; then
-      printf "  %0.2f - %s\n" "$t" "$2"
-    fi
-  fi
+  case "$MODE" in
+    "PROGRESS")
+      if [ $(echo "$t > 0" | bc -l) = "1" ]; then
+        printf "  %0.2f - %s\n" "$t" "$2"
+      fi
+      ;;
+    "SPREADSHEET")
+      if $FIRST; then
+        FIRST=false
+      else
+        printf ", "
+      fi
+      if [ $(echo "$t > 0" | bc -l) = "1" ]; then
+        printf "%0.2f" "$t"
+      fi
+      ;;
+    *)
+      echo "Invalid mode!"
+      exit 1
+      ;;
+  esac
 }
 
-echo "Breakdown:"
+if [ "$MODE" = "PROGRESS" ]; then
+  echo "Breakdown:"
+fi
+
 tally r "Research Solo"
 tally R "Research Confab"
 tally a "Advising Tasks"
@@ -36,7 +54,7 @@ tally S "Service"
 tally M "Management"
 tally m "Misc."
 
-if [ "$MODE" = "TABS" ]; then
+if [ "$MODE" = "SPREADSHEET" ]; then
   echo
 fi
 
