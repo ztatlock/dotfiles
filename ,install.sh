5da592cc4b65763110004e9c6fedd7ff7df60f83
Zachary Tatlock <ztatlock@cs.washington.edu>
2022-08-06 22:14:36 -0400

    clean up

---
+++ ,install.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+###############################################################################
+# exit loudly if anything errors
+###############################################################################
+
+set -o errexit
+set -o errtrace
+
+function fail {
+  echo "
+!!!
+!!! ERROR
+!!!
+" >&2
+  exit 1
+}
+
+trap fail ERR
+trap fail SIGINT
+
+###############################################################################
+# determine this script's directory
+###############################################################################
+
+src="${BASH_SOURCE[0]}"
+while [ -L "$src" ]; do
+  dir="$(cd -P "$(dirname "$src")" && pwd)"
+  src="$(readlink "$src")"
+  [[ $src != /* ]] && src="$dir/$src"
+done
+
+MYDIR="$(cd -P "$(dirname "$src")" && pwd)"
+
+###############################################################################
+# chatty symbolic linker
+###############################################################################
+
+function install {
+  src="$1"
+  tgt="$2"
+
+  echo "$src --> $tgt"
+
+  [ -r "$src" ]
+  rm -f "$tgt"
+  ln -s "$src" "$tgt"
+}
+
+###############################################################################
+# install configs
+###############################################################################
+
+echo
+echo "CONFIGS"
+cd "$MYDIR/configs"
+install "$(pwd)/profile" "$HOME/.profile"
+install "$(pwd)/bash"    "$HOME/.bashrc"
+install "$(pwd)/git"     "$HOME/.gitconfig"
+install "$(pwd)/ssh"     "$HOME/.ssh/config"
+install "$(pwd)/tmux"    "$HOME/.tmux.conf"
+
+###############################################################################
+# install scripts
+###############################################################################
+
+mkdir -p "$HOME/bin"
+
+echo
+echo "SCRIPTS"
+cd "$MYDIR/scripts"
+for s in *; do
+  install "$(pwd)/$s" "$HOME/bin/$(basename "$s")"
+done
