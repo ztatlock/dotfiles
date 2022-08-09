06fd0c3fb4969b538c306a9ae4ccc9ea35747f21
Zachary Tatlock <ztatlock@cs.washington.edu>
2022-08-09 10:08:08 -0400

    fix key forwarding

--- scripts/,ssh-gen-key.sh
+++ scripts/,ssh-gen-key.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# TODO switch to ed25519
+# https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
 echo
 echo "Deleting existing ssh keys."
 read -p "OK (y/n)? " yn
