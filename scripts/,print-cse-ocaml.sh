6b793325b0c035a06350aaf59258ab4bbb2203a9
Zachary Tatlock <ztatlock@cs.washington.edu>
2022-10-04 09:26:10 -0700

    better printing script names

--- scripts/,print-cse-ocaml.sh
+++ scripts/,print-cse-ocaml.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 enscript -2 --landscape --highlight=ocaml "$1" --output=- \
-  | ssh recycle.cs.washington.edu "lpr -Ppg224 -o sides=two-sided-long-edge"
+  | ps2pdf - - \
+  | ,print-cse.sh -
