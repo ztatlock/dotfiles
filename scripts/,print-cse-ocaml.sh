2e1695f14c3273c6caef4aed29853850bf35c4ff
Zachary Tatlock <ztatlock@cs.washington.edu>
2022-11-05 16:50:28 -0700

    fix printing script

--- scripts/,print-cse-ocaml.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-enscript -2 --landscape --highlight=ocaml "$1" --output=- \
-  | ps2pdf - - \
-  | ,print-cse.sh -
--- scripts/,print-cse.sh
+++ scripts/,print-cse.sh
@@ -1,3 +1,48 @@
 #!/usr/bin/env bash
 
-cat "$1" | ssh recycle.cs.washington.edu "lpr -Ppg224 -o sides=two-sided-long-edge"
+set -u # reading an unset variable is an error
+set -e # exit on error
+
+# ensure file passed as arg
+if [ $# -lt 1 ]; then
+  echo
+  echo "Usage: $(basename "$0") [file]"
+  exit -1
+fi
+file="$1"
+if [ ! -f "$file" ]; then
+  echo
+  echo "No such file '$file'"
+  exit -1
+fi
+
+# prompt for which printer
+printers=(pg224 pgc224 pg270 pgc270)
+echo
+for i in ${!printers[@]}; do
+  if [ $i -eq 0 ]; then
+    echo "$i : ${printers[$i]} (default)"
+  else
+    echo "$i : ${printers[$i]}"
+  fi
+done
+echo
+read -p "Print on: " i
+if [ -z "$i" ]; then
+  i=0
+fi
+printer="${printers[$i]}"
+echo "OK, printing on $printer."
+echo
+
+# actually print (two-sided)
+cat "$file" \
+  | ssh recycle.cs.washington.edu "lpr -P${printer} -o sides=two-sided-long-edge"
+
+# offer to remove file
+read -p "Remove file? " yn
+case "$yn" in
+  [Yy]*)
+    rm "$file"
+    ;;
+esac
